a543eb5b59fbb80fb1c306907511d50b31c40fce
Shahriya Siddique <ps.shahriya@gmail.com>
2025-05-07 10:28:39 +0600

    feat: add snippet creation and logout to header, update dashboard header styling

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  protected authService = inject(AuthService);
   isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-} 
+
+  logout() {
+    this.authService.logout();
+  }
+}
